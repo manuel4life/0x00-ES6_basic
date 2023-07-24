352579860f0c3a2ebdccadeb40aa8d8f120e635c
manuel4life <allotey4eva@gmail.com>
2023-07-24 17:40:44 +0000

    made modifications to the code

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,13 +1,9 @@
 import ClassRoom from './0-classroom.js';
 
-function initializeRooms() {
-  const rooms = [
+export default function initializeRooms() {
+   return [
     new ClassRoom(19),
     new ClassRoom(20),
-    new ClassRoom(34)
+    new ClassRoom(34),
   ];
-
-  return rooms;
 }
-
-export default initializeRooms;
