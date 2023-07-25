18ef34b903ef30e2484fda12797f22e7700e9edc
manuel4life <allotey4eva@gmail.com>
2023-07-25 08:27:50 +0000

    corrections of eslint errors

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,7 +1,7 @@
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-   return [
+  return [
     new ClassRoom(19),
     new ClassRoom(20),
     new ClassRoom(34),
