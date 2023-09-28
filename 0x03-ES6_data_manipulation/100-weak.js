25a06ad03d5180e1e33998620448ce7d5aca0aef
manuel4life <allotey4eva@gmail.com>
2023-09-28 09:28:13 +0000

    100-weak.js

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,13 +1,15 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-  if (!weakMap.has(endpoint)) {
-    weakMap.set(endpoint, 1);
-  } else {
-    const queryCount = weakMap.get(endpoint);
-    if (queryCount >= 5) {
-      throw new Error('Endpoint load is high');
-    }
-    weakMap.set(endpoint, queryCount + 1);
+  let called = weakMap.get(endpoint) || 0;
+
+  called += 1;
+
+  weakMap.set(endpoint, called);
+
+  if (called >= 5) {
+    throw Error('Endpoint load is high');
   }
+
+  return called;
 }
